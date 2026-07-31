7aad17269b7e0e4ec459cc46261041df93d1bd87
Daniel Victor <ilauncherdeveloper@gmail.com>
2026-07-31 18:26:17 -0300

    make proxy callbacks public

--- sdk/include/simplejs/proxy.h
+++ sdk/include/simplejs/proxy.h
@@ -62,3 +62,15 @@ _simplejs_proxy_set_name_callback(delete_property);
 
 _simplejs_proxy_set_name_callback(get_string);
 #endif
+
+simplejs_status_t SIMPLEJS_API simplejs_proxy_release(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
+
+simplejs_status_t SIMPLEJS_API simplejs_proxy_lock_property_list(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
+simplejs_status_t SIMPLEJS_API simplejs_proxy_unlock_property_list(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
+simplejs_status_t SIMPLEJS_API simplejs_proxy_query_property(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_proxy_property_query_t *out);
+
+simplejs_status_t SIMPLEJS_API simplejs_proxy_get_property_value(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property, simplejs_variable_t *out);
+simplejs_status_t SIMPLEJS_API simplejs_proxy_set_property_value(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property, simplejs_variable_t *in);
+simplejs_status_t SIMPLEJS_API simplejs_proxy_delete_property(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property);
+
+simplejs_status_t SIMPLEJS_API simplejs_proxy_get_string(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, char **out);
