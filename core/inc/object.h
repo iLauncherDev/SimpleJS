5d715c7d21a2c56382e349f958755e5e6d307e46
Daniel Victor <ilauncherdeveloper@gmail.com>
2026-07-31 18:26:17 -0300

    make proxy callbacks public

--- core/inc/object.h
+++ core/inc/object.h
@@ -29,16 +29,3 @@ struct simplejs_object
 
     simplejs_list_entry_t gc_list_entry;
 };
-
-// proxy functions
-simplejs_status_t simplejs_proxy_release(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
-
-simplejs_status_t simplejs_proxy_lock_property_list(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
-simplejs_status_t simplejs_proxy_unlock_property_list(simplejs_proxy_t *proxy, simplejs_proxy_context_t context);
-simplejs_status_t simplejs_proxy_query_property(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_proxy_property_query_t *out);
-
-simplejs_status_t simplejs_proxy_get_property_value(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property, simplejs_variable_t *out);
-simplejs_status_t simplejs_proxy_set_property_value(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property, simplejs_variable_t *in);
-simplejs_status_t simplejs_proxy_delete_property(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, simplejs_variable_t *property);
-
-simplejs_status_t simplejs_proxy_get_string(simplejs_proxy_t *proxy, simplejs_proxy_context_t context, char **out);
