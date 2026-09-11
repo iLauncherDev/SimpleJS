4cfd59c397f61cb98ff0f60d8d70f9adc5cce56b
Daniel Victor <ilauncherdeveloper@gmail.com>
2026-09-11 20:03:37 -0300

    implement prototype on dynamic_object

--- sdk/include/simplejs/builtin_object/dynamic_object.h
+++ sdk/include/simplejs/builtin_object/dynamic_object.h
@@ -2,4 +2,5 @@
 #include "../object.h"
 
 simplejs_status_t SIMPLEJS_API simplejs_builtin_create_dynamic_object(simplejs_object_t **out);
+void SIMPLEJS_API simplejs_builtin_set_dynamic_object_prototype(simplejs_object_t *object, simplejs_variable_t *prototype_value);
 void SIMPLEJS_API simplejs_builtin_set_dynamic_object_read_only(simplejs_object_t *object, bool read_only);
