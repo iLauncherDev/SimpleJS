11edc4238f8bd84d527e22941a257e4b4bb6cdcb
Daniel Victor <ilauncherdeveloper@gmail.com>
2026-09-11 20:03:37 -0300

    implement prototype on dynamic_object

--- core/inc/builtin_object/dynamic_object.h
+++ core/inc/builtin_object/dynamic_object.h
@@ -4,6 +4,8 @@
 
 typedef struct simplejs_dynamic_object_property
 {
+    bool is_hardcoded;
+
     simplejs_proxy_property_t property;
 
     simplejs_safe_list_entry_t safe_list_entry;
@@ -11,7 +13,12 @@ typedef struct simplejs_dynamic_object_property
 
 typedef struct simplejs_dynamic_object_raw
 {
+    simplejs_object_t *this_object;
+
+    simplejs_dynamic_object_property_t prototype_property;
+
     simplejs_safe_list_t property_list;
+
     bool read_only;
 } simplejs_dynamic_object_raw_t;
 
